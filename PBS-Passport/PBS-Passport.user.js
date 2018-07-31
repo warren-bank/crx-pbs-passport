9329972c74336f870407924e56127d31bede25f3
Warren R Bank <warren.r.bank@gmail.com>
2018-07-30 17:21:00 -0700

    adjust for recent changes to PBS website

--- PBS-Passport/PBS-Passport.user.js
+++ PBS-Passport/PBS-Passport.user.js
@@ -14,15 +14,15 @@ var payload = function(){
   }
   else {
     try {
-      var vb, video_MP4, video_M3U8, encoding, url, source, body
+      var vb, encodings, video_MP4, video_M3U8, url, source, body
 
       vb = window.videoBridge
       if (vb){
+        encodings = vb.encodings
 
         // .mp4
-        encoding = vb.alternate_encoding
-        if (encoding && encoding.url){
-          url = encoding.url
+        if (encodings && encodings.length > 1){
+          url = encodings[1]
 
           // create <video> element
           video_MP4 = document.createElement('video')
@@ -37,9 +37,8 @@ var payload = function(){
         }
 
         // .m3u8
-        encoding = vb.recommended_encoding
-        if (encoding && encoding.url){
-          url = encoding.url
+        if (encodings && encodings.length > 0){
+          url = encodings[0]
 
           // create <video> element
           video_M3U8 = document.createElement('video')
@@ -53,7 +52,7 @@ var payload = function(){
 
         if (video_MP4 || video_M3U8){
           // update <body> element
-          body = window.document.getElementsByTagName('body')[0]
+          body = document.getElementsByTagName('body')[0]
           while (body.firstChild) body.removeChild(body.firstChild)
 
           if (video_MP4)  body.appendChild(video_MP4)
