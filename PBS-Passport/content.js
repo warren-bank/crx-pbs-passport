6473fe2ab113ad1fc498c4cd48375283d0912cc7
Warren R Bank <warren.r.bank@gmail.com>
2018-08-01 00:41:18 -0700

    display links under the video
    caveats:
    * due to: URL redirects, CORS headers, and same-origin restrictions
      * the webcast links won't load without assistance
      * the default configuration of my "CORS Lite" extension is a workaround
        * https://github.com/warren-bank/crx-cors-lite/releases

--- PBS-Passport/content.js
+++ PBS-Passport/content.js
@@ -5,6 +5,42 @@ var payload = function(){
   else {
     try {
       var vb, encodings, video_MP4, video_M3U8, url, source, body
+      var video_links = {direct: {}, webcast: {}}
+
+      var add_video_link_captions = function(c_type, c_url) {
+        video_links.direct[c_type] = c_url
+
+        ;['MP4', 'M3U8'].forEach(function(v_type){
+          var v_url = video_links.direct[v_type]
+          if (v_url) {
+            video_links.webcast[v_type + ' + ' + c_type] = 'https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html#/watch/' + window.btoa(v_url) + '/subtitle/' + window.btoa(c_url)
+          }
+        })
+      }
+
+      var display_video_links = function() {
+        var html, div, body
+
+        html = ''
+        ;['direct', 'webcast'].forEach(function(heading, index){
+          if (index) html += '<hr />'
+
+          html += '<h3>' + heading + '</h3>'
+
+          html += '<ul>'
+          for (var key in video_links[heading]) {
+            html += `<li><a target="_blank" href="${video_links[heading][key]}">${key}</a></li>`
+          }
+          html += '</ul>'
+        })
+
+        div = document.createElement('div')
+        div.innerHTML = html
+        div.style.margin = '20px'
+
+        body = document.getElementsByTagName('body')[0]
+        body.appendChild(div)
+      }
 
       vb = window.videoBridge
       if (vb){
@@ -24,6 +60,9 @@ var payload = function(){
           source.src = url
 
           video_MP4.appendChild(source)
+
+          video_links.direct['MP4'] = url
+          video_links.webcast['MP4'] = 'https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html#/watch/' + window.btoa(url)
         }
 
         // .m3u8
@@ -38,15 +77,35 @@ var payload = function(){
           source.src = url
 
           video_M3U8.appendChild(source)
+
+          video_links.direct['M3U8'] = url
+          video_links.webcast['M3U8'] = 'https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html#/watch/' + window.btoa(url)
         }
 
         if (video_MP4 || video_M3U8){
-          // update <body> element
+          // add subtitles to "video_links" data structure
+          encodings = vb.cc
+
+          // .vtt
+          if (encodings && encodings.WebVTT){
+            add_video_link_captions('WebVTT', encodings.WebVTT)
+          }
+
+          // .srt
+          if (encodings && encodings.SRT){
+            add_video_link_captions('SRT', encodings.SRT)
+          }
+
+          // update DOM: remove all previous content
           body = document.getElementsByTagName('body')[0]
           while (body.firstChild) body.removeChild(body.firstChild)
 
+          // update DOM: add video elements
           if (video_MP4)  body.appendChild(video_MP4)
           if (video_M3U8) body.appendChild(video_M3U8)
+
+          // update DOM: print "video_links" data structure
+          display_video_links()
         }
       }
     }
